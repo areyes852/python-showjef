436255b69bac292303a90148b270982b182f6e73
David Boddie <david@boddie.org.uk>
2009-09-11 02:33:28 +0200

    Started to collate colours, using colour charts to convert between thread colour codes and RGB values, and by matching the internal colour codes found in files to the colour codes shown in colour charts.

---
+++ Colours/jef_colours.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import sys
+
+import all_colours
+import measured_colours
+import internal_colours
+
+known_colours = all_colours.groups
+
+colours = {}
+
+# Examine the dictionaries mapping internal colour codes to other colour codes,
+# looking up each code in the dictionaries mapping colour codes to known
+# colours.
+
+for group in internal_colours.groups.keys():
+
+    if known_colours.has_key(group):
+    
+        colours_dict = known_colours[group]
+        
+        for internal_code, other_code in internal_colours.groups[group].items():
+        
+            if colours.has_key(internal_code):
+                continue
+            
+            try:
+                colours[internal_code] = colours_dict[other_code]
+            except KeyError:
+                pass
+
+
+def colour(identifier):
+
+    try:
+        name, rgb = colours[identifier]
+    except KeyError:
+        name, rgb = measured_colours.colours[identifier]
+    
+    return int(rgb[1:3], 16), int(rgb[3:5], 16), int(rgb[5:7], 16)
--- jef.py
+++ jef.py
@@ -21,58 +21,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import struct, sys, time
 
-ColorTable = {
-    0x01: (0, 0, 0), # Black
-    0x02: (254, 254, 254), # see table
-    0x03: (249, 237, 5), # Gold Yellow
-    0x05: (67, 86, 7), # see table
-    0x06: (49, 125, 37), # see table
-    0x08: (146, 96, 172), # see table
-    0x0a: (254, 0, 0), # see table
-    0x0b: (180, 78, 100), # see table
-    0x0c: (35, 54, 114), # see table
-    0x0d: (227, 243, 93), # see table
-    0x0e: (68, 35, 4), # see table
-    0x0f: (195, 189, 191), # some kind of silver (estimated)
-    0x10: (255, 227, 197), # see table
-    0x12: (244, 165, 132), # see table
-    0x13: (184, 176, 139), # some kind of tan (estimated)
-    0x14: (147, 5, 3), # see table
-    0x15: (177, 176, 211), # see table
-    0x17: (250, 218, 223), # see table
-    0x18: (162, 162, 190), # some kind of light blue (estimated)
-    0x1a: (155, 217, 235), # see table
-    0x1c: (101, 154, 214), # see table
-    0x1f: (196, 126, 114), # some kind of dark pink (estimated)
-    0x20: (255, 0, 0),
-    0x21: (133, 0, 0), # some kind of dark red (estimated)
-    0x22: (201, 172, 138), # some kind of tan/skin (estimated)
-    0x24: (200, 160, 0),
-    0x28: (200, 0, 0),
-    0x29: (78, 41, 143), # see table
-    0x2b: (185, 185, 185), # some kind of grey (estimated)
-    0x2c: (0, 0, 0), # black (estimated)
-    0x2d: (125, 112, 2), # see table
-    0x2e: (2, 56, 34), # see table
-    0x32: (80, 84, 87), # see table
-    0x34: (198, 98, 0), # see table
-    0x35: (212, 165, 95), # see table
-    0x36: (250, 127, 126), # see table
-    0x37: (0, 0, 0),
-    0x38: (140, 18, 55), # some kind of red (estimated)
-    0x3a: (105, 45, 4), # see table
-    0x3c: (0, 0, 255),
-    0x3e: (158, 214, 125), # see table
-    0x3f: (240, 77, 140), # see table
-    0x41: (194, 47, 75), # some kind of red (estimated)
-    0x43: (0, 150, 0), # see table
-    0x44: (255, 240, 142), # see table
-    0x45: (185, 153, 2), # see table
-    0x47: (88, 156, 83), # some kind of green (estimated)
-    0x48: (252, 179, 68), # see table
-    0x4d: (198, 190, 194), # see table
-    0x4e: (202, 156, 106), # some kind of tan/skin (estimated)
-    }
+from Colours import jef_colours
 
 class Pattern:
 
@@ -163,7 +112,7 @@ class Pattern:
     
         try:
             identifier = self.colours[index]
-            colour = ColorTable[identifier]
+            colour = jef_colours.colour(identifier)
         except KeyError:
             colour = (0, 0, 0)
             sys.stderr.write("Failed to find colour 0x%02x (%i).\n" % (identifier, identifier))
