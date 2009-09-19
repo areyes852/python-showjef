aa7d60d415d233a4dc4369670e29302ba467fe99
David Boddie <david@boddie.org.uk>
2009-09-19 03:16:05 +0200

    Removed unnecessary imports.

--- jef2svg.py
+++ jef2svg.py
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os, struct, sys
+import os, sys
 from PyQt4.QtCore import QRect, QT_VERSION
 from PyQt4.QtGui import *
 from PyQt4.QtSvg import QSvgGenerator
